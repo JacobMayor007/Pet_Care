ed1c58e113cc4d7bb3c35dccddbcd96f9e6f60b5
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-03-02 14:22:18 +0800

    Renter's Page Done

--- src/app/SignedOut/page.tsx
+++ src/app/SignedOut/page.tsx
@@ -1,32 +1,27 @@
-import { auth } from "../firebase/config";
+import { getAuth, signOut } from "firebase/auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signout() {
-  const signOut = async () => {
-    const user = auth;
+  const router = useRouter();
+
+  const signOutFunction = async () => {
+    const auth = getAuth();
     try {
-      await user
-        .signOut()
-        .then(() => {
-          localStorage.clear();
-          console.log("Signed out successfully.");
-        })
-        .catch((error) => {
-          console.error("Error signing out: ", error);
-        });
+      await signOut(auth).then(() => {
+        router.push("/Login");
+      });
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    auth.currentUser && (
-      <button
-        className="w-24 h-10 bg-blue-950 text-white text-base font-hind font-medium tracking-wide rounded-md drop-shadow-lg"
-        onClick={() => signOut}
-      >
-        <Link href="/Login">Sign Out</Link>
-      </button>
-    )
+    <button
+      className="w-24 h-10 bg-blue-950 text-white text-base font-hind font-medium tracking-wide rounded-md drop-shadow-lg"
+      onClick={() => signOutFunction}
+    >
+      <Link href="/Login">Sign Out</Link>
+    </button>
   );
 }
