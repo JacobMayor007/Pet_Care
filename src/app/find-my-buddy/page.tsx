00922a92bb9f3f353cfcffb75bf1d107973fb5a5
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-03-09 09:58:35 +0800

    FindMyBuddy, FrontEnd Done!

--- src/app/find-my-buddy/page.tsx
+++ src/app/find-my-buddy/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faStar, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEyeSlash,
+  faHeart,
+  faStar,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   BellOutlined,
   ShoppingCartOutlined,
@@ -11,7 +16,6 @@ import { useEffect, useRef, useState } from "react";
 import fetchUserData from "../fetchData/fetchUserData";
 import Link from "next/link";
 import Signout from "../SignedOut/page";
-import { UserNotification } from "../ClientNavbar/page";
 import Image from "next/image";
 
 interface Notifications {
@@ -248,3 +252,99 @@ export default function FoundMyBuddy() {
     </div>
   );
 }
+
+const UserNotification = () => {
+  const [myNotification, setMyNotification] = useState<Notifications[]>([]);
+
+  useEffect(() => {
+    let unsubscribe: () => void;
+
+    const getMyNotifications = async () => {
+      try {
+        const data = await fetchUserData();
+        const userUID = data[0]?.User_UID;
+
+        if (!userUID) {
+          console.log("Logged In First");
+          return;
+        }
+
+        unsubscribe = Notifications.default(userUID, (newNotif) => {
+          setMyNotification(newNotif);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getMyNotifications();
+
+    // Cleanup listener on unmount
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="max-w-[500px] w-[482px] h-fit max-h-[542px] bg-white drop-shadow-lg rounded-xl justify-self-center flex flex-col  overflow-y-scroll">
+      <h1 className="font-hind text-lg mx-4 mt-4 mb-2">Notifications</h1>
+      <div className="h-0.5 border-[#393939] w-full border-[1px] mb-2" />
+      {myNotification.map((data) => {
+        return (
+          <div
+            key={data?.id}
+            className=" drop-shadow-lg grid grid-cols-12 p-2 items-center"
+          >
+            <div className="m-2 h-2 w-2 rounded-full bg-blue-400 animate-pulse" />
+            <div className="grid grid-cols-12 my-2 col-span-11">
+              <a
+                href={`/pc/${
+                  data?.appointment_ID
+                    ? `schedule/${data.appointment_ID}`
+                    : data?.order_ID
+                    ? `cart/${data.order_ID}`
+                    : `rooms/${data?.room_ID}`
+                }`}
+                className="col-span-11 grid grid-cols-12"
+              >
+                <div className="h-12 w-12 col-span-2 rounded-full bg-white drop-shadow-lg font-montserrat text-xs flex items-center justify-center text-center text-nowrap overflow-hidden">
+                  Image of <br />
+                  Pet
+                </div>
+                <div className="flex flex-col gap-1 font-montserrat text-wrap col-span-10 text-sm">
+                  <h1 className="text-[#393939] font-medium">
+                    {data?.message}
+                  </h1>
+                  <p className="text-xs text-[#797979]">{data?.createdAt}</p>
+                </div>
+              </a>
+              <div className="flex justify-center mt-0.5 ">
+                <FontAwesomeIcon icon={faEyeSlash} />
+              </div>
+            </div>
+            {data?.type === "Change Appointment" ? (
+              <div className="flex flex-row justify-end gap-4 col-span-12 mr-10">
+                <button
+                  type="button"
+                  className="bg-[#61C4EB] py-1 px-5 text-white rounded-lg"
+                >
+                  Accept
+                </button>
+                <button
+                  type="button"
+                  className="bg-red-400 py-1 px-5 text-white rounded-lg"
+                >
+                  Reject
+                </button>
+              </div>
+            ) : (
+              <div className="hidden" />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
